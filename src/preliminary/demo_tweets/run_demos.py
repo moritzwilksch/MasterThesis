814e38aa5e6be862d966d7bb055444bccbe4396c
Moritz Wilksch <moritzwilksch@gmail.com>
2022-03-20 10:58:17 +0100

    change demos to read+write from file

--- src/preliminary/demo_tweets/run_demos.py
+++ src/preliminary/demo_tweets/run_demos.py
@@ -1,34 +1,42 @@
+import yaml
 from rich.console import Console
 from rich.panel import Panel
 
-from src.preliminary.demo_tweets.models import (FinBERT, ModelWrapper,
-                                                TwitterRoberta, Vader)
+from src.preliminary.demo_tweets.models import (
+    FinBERT,
+    ModelWrapper,
+    TwitterRoberta,
+    Vader,
+)
 
-c = Console()
+c = Console(record=True)
 c.clear()
 
 
 class Experiment:
-    def __init__(self, models: list[ModelWrapper], texts: list[str]):
+    def __init__(self, models: list[ModelWrapper], examples_filename: str):
         self.models = models
-        self.texts = texts
+
+        with open("data/examples/examples.yaml") as f:
+            self.examples = yaml.safe_load(f)
 
     def run(self) -> None:
-        for text in self.texts:
-            c.print(Panel(text))
-            for model in self.models:
-                pred = model.predict(text)
-                c.print(f"{f'{model.__class__.__name__}:':<25} {pred}")
+        for category in self.examples:
+            c.print(Panel(category.upper()), style="bold")
+            texts = self.examples.get(category)
+            for text in texts:
+                c.print(text, style="bold")
+                for model in self.models:
+                    pred = model.predict(text)
+                    c.print(f"\t{f'{model.__class__.__name__}:':<25} {pred}")
+                c.print()
 
 
 experiment = Experiment(
     models=[TwitterRoberta(), FinBERT(), Vader()],
-    texts=[
-        "Today is tuesday.",
-        "Had a great day today! #happy #good #great #greatday #goodday #gooddaytoday #gooddaytoday",
-        "What a miserable shitty day! #sad #bad #badday #baddaytoday #baddaytoday",
-        "Stocks rallied and the British pound gained.",
-        "Stocks deep red and the British pound lost 5%...",
-    ],
+    examples_filename="data/examples/examples.yaml",
 )
 experiment.run()
+
+with open("outputs/examples/examples.txt", "w") as f:
+    f.writelines(c.export_text())
