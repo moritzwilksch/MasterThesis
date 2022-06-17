3327c013cf6da1528650219200810ec2d5c84273
Moritz Wilksch <moritzwilksch@gmail.com>
2022-06-17 17:00:06 +0000

    make plots nice

--- src/dl_modeling/inference.py
+++ src/dl_modeling/inference.py
@@ -8,36 +8,47 @@ from sklearn.metrics import classification_report, confusion_matrix, roc_auc_sco
 from src.dl_modeling.data import TweetDataModule
 from src.dl_modeling.models import RecurrentSAModel, TransformerSAModel
 from src.utils.preprocessing import Preprocessor
-
+import time
 #%%
-data = TweetDataModule(split_idx="retrain", batch_size=64, model_type="transformer")
+USE_MODEL = "recurrent"
+
 prepper = Preprocessor()
-# model = RecurrentSAModel.load_from_checkpoint(
-#     "lightning_logs/recurrent-split-0/epoch=22-val_acc=0.63.ckpt"
-# )
-model = TransformerSAModel.load_from_checkpoint(
-    "outputs/models/transformer_final.ckpt"
-)
-model.eval()
-
+if USE_MODEL == "transformer":
+    data = TweetDataModule(split_idx="retrain", batch_size=64, model_type="transformer")
+    model = TransformerSAModel.load_from_checkpoint(
+        "outputs/models/transformer_final.ckpt"
+    )
+    model.eval()
+else:
+    data = TweetDataModule(split_idx="retrain", batch_size=64, model_type="recurrent")
+    model = RecurrentSAModel.load_from_checkpoint(
+        "outputs/models/gru_final.ckpt"
+    )
 
 #%%
-s = "short $TSLA, buy puts"
-# s = "long $TSLA, buy calls"
-s = "$OXY just laid off 40% of staff"
+# s = "short $TSLA, buy puts"
+# # s = "long $TSLA, buy calls"
+# s = "$OXY just laid off 40% of staff"
 
-df = prepper.process(pl.DataFrame({"text": [s]}))
-# x = torch.Tensor(data.vocab(data.tokenizer(df["text"][0]))).long().reshape(-1, 1)
-x = torch.Tensor(list(data.tokenizer(df["text"]))[0]).long().reshape(-1, 1)
+# df = prepper.process(pl.DataFrame({"text": [s]}))
+# # x = torch.Tensor(data.vocab(data.tokenizer(df["text"][0]))).long().reshape(-1, 1)
+# x = torch.Tensor(list(data.tokenizer(df["text"]))[0]).long().reshape(-1, 1)
 
-# F.softmax(model(x, seq_lens=[len(x)]), dim=0)
-out = model(x, masks=torch.full((len(x),), False).unsqueeze(0))
-F.softmax(out, dim=1)
+# # F.softmax(model(x, seq_lens=[len(x)]), dim=0)
+# out = model(x, masks=torch.full((len(x),), False).unsqueeze(0))
+# F.softmax(out, dim=1)
 
 #%%
 test = data.test_dataloader()
 trainer = ptl.Trainer()
+
+tic = time.perf_counter()
 batched_preds = trainer.predict(model, test)
+tac = time.perf_counter()
+
+time_taken = tac - tic  # to make comparable: this should be inference time for 2_000 samples
+time_taken / len(data.ytest) * 2_000
+print(f"Prediction time for 2000 samples: {tac - tic}")
 
 #%%
 preds = torch.vstack(batched_preds)  # .argmax(dim=1)
--- src/interactive/plotting/benchmark_plots.py
+++ src/interactive/plotting/benchmark_plots.py
@@ -8,87 +8,179 @@ import toml
 from src.utils.plotting import Colors, set_style
 
 set_style()
+OURS = ["LogReg", "SVM", "TransformerNN", "RecurrentNN"]
+PRETRAINED = ["FinBERT", "TwitterRoBERTa", "VADER", "NTUSD-Fin"]
 #%%
 data = toml.load("outputs/static/model_test_scores.toml")
 
-scores = pd.DataFrame({k: data[k]["test_scores"] for k in data}).drop("SVM", axis=1)
-scores_melted = scores.melt(var_name="model", value_name="score")
+scores = pd.DataFrame({k: data[k]["test_scores"] for k in data})
 
-times = pd.DataFrame({k: data[k]["times_taken"] for k in data}).drop("SVM", axis=1)
+times = pd.DataFrame({k: data[k]["times_taken"] for k in data})
 times = times / 2000 * 1000  # per-sample in ms!!!
-times_melted = times.melt(var_name="model", value_name="time")
 
 #%%
-fig, ax = plt.subplots(figsize=(12, 5))
-sns.pointplot(
-    data=scores_melted,
-    x="model",
-    y="score",
-    order=scores.mean().sort_values().index,
-    color=Colors.DARKBLUE.value,
-    linestyles=":",
-    join=False,
-    # capsize=0.05,
-    ci="sd",
-    ax=ax,
-    zorder=10,
-)
+def plot_scores(score_df, ax):
+    scores_melted = score_df.melt(var_name="model", value_name="score")
+    sns.pointplot(
+        data=scores_melted,
+        x="model",
+        y="score",
+        order=score_df.mean().sort_values().index,
+        palette=[Colors.YELLOW.value] * len(PRETRAINED)
+        + [Colors.DARKBLUE.value] * len(OURS),
+        linestyles=":",
+        join=False,
+        # capsize=0.05,
+        scale=1.1,
+        ci="sd",
+        ax=ax,
+        zorder=10,
+    )
 
-ax.plot(
-    scores.mean().sort_values().index,
-    scores.mean().sort_values().values,
-    color=Colors.DARKBLUE.value,
-    alpha=0.5,
-    ls="-",
-    zorder=0,
-)
+    ax.plot(
+        score_df.mean().sort_values().index[: len(PRETRAINED)],
+        score_df.mean().sort_values().values[: len(PRETRAINED)],
+        color=Colors.YELLOW.value,
+        ls="-",
+        lw=3,
+        zorder=0,
+        label="existing models",
+    )
 
-ax.grid(axis="y", ls="--", color="black", alpha=0.25)
-ax.set_ylim(0.49, 0.85)
-ax.set_xlim(-0.25, 5.25)
-ax.set_xlabel("Model", weight="bold", labelpad=15)
+    ax.plot(
+        score_df.mean().sort_values().index[len(PRETRAINED) :],
+        score_df.mean().sort_values().values[len(PRETRAINED) :],
+        color=Colors.DARKBLUE.value,
+        ls="-",
+        lw=3,
+        zorder=0,
+        label="proposed models",
+    )
+
+    # ax.legend(framealpha=1)
+    ax.text(
+        x=1.5,
+        y=0.87,
+        s="Existing Models",
+        weight="bold",
+        color=Colors.YELLOW.value,
+        ha="center",
+    )
+    ax.text(
+        x=5.5,
+        y=0.87,
+        s="Proposed Models",
+        weight="bold",
+        color=Colors.DARKBLUE.value,
+        ha="center",
+    )
+
+
+fig, ax = plt.subplots(figsize=(15, 5))
+plot_scores(scores, ax)
+
+
+ax.set_ylim(0.49, 0.86)
 ax.set_ylabel("Out-of-sample ROC AUC", labelpad=15, weight="bold")
-
 ax.tick_params(axis="x", length=0)
-# ax.set_xticklabels(ax.get_xticklabels(), weight="bold")
+ax.set_xlim(-0.25, 7.25)
+ax.set_xlabel("Model", weight="bold", labelpad=15)
+ax.grid(axis="y", ls="--", color="black", alpha=0.25)
+
 sns.despine(bottom=True)
 plt.tight_layout()
+
 fig.savefig(
     "outputs/plots/model_performance_pyfin.pdf", bbox_inches="tight", facecolor="white"
 )
 
+
 #%%
-fig, ax = plt.subplots(figsize=(12, 5))
-sns.pointplot(
-    data=times_melted,
-    x="model",
-    y="time",
-    order=times.mean().sort_values().index,
-    color=Colors.DARKBLUE.value,
-    linestyles=":",
-    join=False,
-    # capsize=0.05,
-    ci="sd",
-    ax=ax,
-    zorder=10,
-)
+def plot_times(times_df, ax):
+    times_melted_pretrained = times_df.melt(var_name="model", value_name="time").query(
+        "model in @PRETRAINED"
+    )
+    times_melted_ours = times_df.melt(var_name="model", value_name="time").query(
+        "model in @OURS"
+    )
 
-ax.plot(
-    times.mean().sort_values().index,
-    times.mean().sort_values().values,
-    color=Colors.DARKBLUE.value,
-    alpha=0.5,
-    ls="-",
-    zorder=0,
-)
+    times_melted = times_df.melt(var_name="model", value_name="time")
 
-ax.grid(axis="y", ls="--", color="black", alpha=0.25)
+    sns.pointplot(
+        data=times_melted_pretrained,
+        x="model",
+        y="time",
+        order=times[PRETRAINED].mean().sort_values().index.to_list()
+        + times[OURS].mean().sort_values().index.to_list(),
+        color=Colors.YELLOW.value,
+        join=False,
+        ci="sd",
+        ax=ax,
+        zorder=10,
+    )
+
+    sns.pointplot(
+        data=times_melted_ours,
+        x="model",
+        y="time",
+        order=times[PRETRAINED].mean().sort_values().index.to_list()
+        + times[OURS].mean().sort_values().index.to_list(),
+        color=Colors.DARKBLUE.value,
+        join=False,
+        ci="sd",
+        ax=ax,
+        zorder=10,
+    )
+
+    ax.plot(
+        times_df[PRETRAINED].mean().sort_values().index,
+        times_df[PRETRAINED].mean().sort_values().values,
+        color=Colors.YELLOW.value,
+        ls="-",
+        lw=3,
+        zorder=0,
+    )
+
+    ax.plot(
+        times_df[OURS].mean().sort_values().index,
+        times_df[OURS].mean().sort_values().values,
+        color=Colors.DARKBLUE.value,
+        ls="-",
+        lw=3,
+        zorder=0,
+    )
+
+    ax.legend(framealpha=1)
+    ax.text(
+        x=1.5,
+        y=200,
+        s="Existing Models",
+        weight="bold",
+        color=Colors.YELLOW.value,
+        ha="center",
+    )
+    ax.text(
+        x=5.5,
+        y=200,
+        s="Proposed Models",
+        weight="bold",
+        color=Colors.DARKBLUE.value,
+        ha="center",
+    )
+
+
+fig, ax = plt.subplots(figsize=(15, 5))
+
+plot_times(times, ax)
 
 ax.set_xlabel("Model", labelpad=15, weight="bold")
 ax.set_ylabel("Inference time per sample (ms)", labelpad=15, weight="bold")
 ax.set_yscale("log")
-ax.yaxis.set_major_formatter(lambda x, pos: f"{x:.1f}")
 ax.tick_params(axis="x", length=0)
+ax.yaxis.set_major_formatter(lambda x, pos: f"{x:.1f}")
+
+ax.grid(axis="y", ls="--", color="black", alpha=0.25)
+ax.legend(frameon=False)
 sns.despine(bottom=True)
 plt.tight_layout()
 fig.savefig(
