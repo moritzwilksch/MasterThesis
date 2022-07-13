7bd9435ebabcf584605dc944efbfa7754a53a5a1
Moritz Wilksch <moritzwilksch@gmail.com>
2022-07-13 12:26:23 +0000

    neutral class analysis

--- src/post_hoc/spam_classifier.py
+++ src/post_hoc/spam_classifier.py
@@ -15,15 +15,15 @@ from src.utils.preprocessing import Preprocessor
 #%%
 df = pd.read_parquet("data/labeled/labeled_tweets.parquet")
 CLASS_MAPPING = {
-    "0": "nospam",
-    "1": "nospam",
-    "2": "spam",
-    "3": "nospam",
+    "0": "some_sentiment",
+    "1": "some_sentiment",
+    "2": "no_sentiment",
+    "3": "some_sentiment",
 }
 prepper = Preprocessor()
 df = prepper.process(pl.DataFrame(df))
 df = df.to_pandas()
-# df = df.assign(label=df["label"].map(CLASS_MAPPING).astype("category"))
+df = df.assign(label=df["label"].map(CLASS_MAPPING).astype("category"))
 
 X = df["text"]
 y = df["label"]
@@ -32,8 +32,13 @@ y = df["label"]
 #%%
 pipe = Pipeline(
     [
-        ("vectorizer", TfidfVectorizer(analyzer="char_wb", ngram_range=(4, 4))),
-        ("model", LogisticRegression(C=1.7, max_iter=450, n_jobs=-1)),
+        (
+            "vectorizer",
+            TfidfVectorizer(
+                analyzer="char_wb", ngram_range=(4, 4), min_df=1.021498227938179e-06
+            ),
+        ),
+        ("model", LogisticRegression(C=1.4281305851678692, max_iter=450, n_jobs=-1)),
     ]
 )
 
@@ -61,5 +66,32 @@ preds = cross_val_predict(pipe, X, y, method="predict")
 
 #%%
 from sklearn.metrics import classification_report
+
 print(classification_report(y, preds))
 # print(roc_auc_score(y.values, preds[:, 1]))
+
+#%%
+def classification_report_to_tex(cr):
+    lines = [
+        " & "
+        + " & ".join(
+            [
+                "\\textbf{Precision}",
+                "\\textbf{Recall}",
+                "\\textbf{F1-Score}",
+                "\\textbf{Support}",
+            ]
+        )
+    ]
+    for cat, data in cr.items():
+        if isinstance(data, float):
+            continue
+
+        lines.append(
+            f"\\textbf{{{cat}}} & {data['precision']:.3f} & {data['recall']:.3f} & {data['f1-score']:.3f} & {data['support']:,d}"
+        )
+
+    return "\\\\\n".join(lines) + "\\\\"
+
+#%%
+print(classification_report_to_tex(classification_report(y, preds, output_dict=True)))
