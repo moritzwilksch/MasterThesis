c4de60a2a175c997d32d93db79064a157bc9f8bf
Moritz Wilksch <moritzwilksch@gmail.com>
2022-07-25 07:20:36 +0000

    finish active learning experiment

--- src/experimental/active_learning2.0.py
+++ src/experimental/active_learning2.0.py
@@ -7,6 +7,12 @@ from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from src.utils.plotting import set_style
+import ray
+
+set_style()
 
 all_data = (
     pd.read_parquet("data/labeled/labeled_tweets.parquet")
@@ -23,39 +29,102 @@ def entropy(y):
 
 
 #%%
+from enum import Enum
 
-pipe = Pipeline(
-    [
-        ("vectorizer", TfidfVectorizer(analyzer="char_wb", ngram_range=(4, 4))),
-        ("model", LogisticRegression(C=1.7, max_iter=350, n_jobs=-1)),
-    ]
-)
 
-train_aucs = []
-test_aucs = []
+class SamplingStrategy(Enum):
+    random = "random"
+    entropy = "entropy"
 
-unlabeled = train_data[train_data["pseudo_label"].isnull()]
-to_be_labeled_idx = unlabeled.sample(500).index
 
-for ii in range(10):
+@ray.remote(num_returns=2)
+def run_experiment(
+    train_data: pd.DataFrame,
+    strategy: SamplingStrategy = SamplingStrategy.random,
+    batch_size: int = 500,
+) -> tuple[list, list]:
+    train_data = train_data.copy()
 
-    train_data.loc[to_be_labeled_idx, "pseudo_label"] = train_data.loc[
-        to_be_labeled_idx, "label"
-    ]
-
-    labeled_data = train_data.loc[~train_data["pseudo_label"].isna()]
-    pipe.fit(labeled_data["text"], labeled_data["pseudo_label"])
-
-    train_preds = pipe.predict_proba(labeled_data["text"])
-    train_aucs.append(
-        roc_auc_score(labeled_data["pseudo_label"], train_preds, multi_class="ovr")
+    pipe = Pipeline(
+        [
+            ("vectorizer", TfidfVectorizer(analyzer="char_wb", ngram_range=(4, 4))),
+            ("model", LogisticRegression(C=1.7, max_iter=350, n_jobs=-1)),
+        ]
     )
 
-    test_preds = pipe.predict_proba(test_data["text"])
-    test_aucs.append(roc_auc_score(test_data["label"], test_preds, multi_class="ovr"))
+    train_aucs = []
+    test_aucs = []
 
-    train_data = train_data.assign(entropy=entropy(pipe.predict_proba(train_data["text"])))
-    to_be_labeled_idx = train_data[train_data["pseudo_label"].isnull()].sample(500).index
+    unlabeled = train_data[train_data["pseudo_label"].isnull()]
+    to_be_labeled_idx = unlabeled.sample(batch_size).index
 
-print(train_aucs)
-print(test_aucs)
+    for ii in range(8_000 // batch_size - 1):
+
+        train_data.loc[to_be_labeled_idx, "pseudo_label"] = train_data.loc[
+            to_be_labeled_idx, "label"
+        ]
+
+        labeled_data = train_data.loc[~train_data["pseudo_label"].isna()]
+        pipe.fit(labeled_data["text"], labeled_data["pseudo_label"])
+
+        train_preds = pipe.predict_proba(labeled_data["text"])
+        train_aucs.append(
+            roc_auc_score(labeled_data["pseudo_label"], train_preds, multi_class="ovr")
+        )
+
+        test_preds = pipe.predict_proba(test_data["text"])
+        test_aucs.append(roc_auc_score(test_data["label"], test_preds, multi_class="ovr"))
+
+        train_data = train_data.assign(
+            entropy=entropy(pipe.predict_proba(train_data["text"]))
+        )
+
+        if strategy == SamplingStrategy.random:
+            to_be_labeled_idx = (
+                train_data[train_data["pseudo_label"].isnull()].sample(batch_size).index
+            )
+        elif strategy == SamplingStrategy.entropy:
+            to_be_labeled_idx = (
+                train_data[train_data["pseudo_label"].isnull()]
+                .sort_values("entropy", ascending=False)
+                .index[:batch_size]
+            )
+
+    return train_aucs, test_aucs
+
+
+#%%
+ray.init(ignore_reinit_error=True)
+
+BATCH_SIZE = 100
+results_random = run_experiment.remote(
+    train_data, SamplingStrategy.random, batch_size=BATCH_SIZE
+)
+results_entropy = run_experiment.remote(
+    train_data, SamplingStrategy.entropy, batch_size=BATCH_SIZE
+)
+
+#%%
+train_aucs_random, test_aucs_random = ray.get(results_random)
+train_aucs_entropy, test_aucs_entropy = ray.get(results_entropy)
+
+#%%
+fig, ax = plt.subplots(1, 1, figsize=(10, 5))
+ax.plot(
+    np.arange(len(test_aucs_random)) * BATCH_SIZE,
+    test_aucs_random,
+    label="Random",
+    color="k",
+)
+ax.plot(
+    np.arange(len(test_aucs_entropy)) * BATCH_SIZE,
+    test_aucs_entropy,
+    label="Entropy",
+    color="blue",
+)
+ax.set_title("Random vs. Entropy Sampling (batch_size = 100)", weight="bold")
+ax.set_xlabel("Number of labeled samples", weight="bold")
+ax.set_ylabel("Test AUC", weight="bold")
+ax.legend()
+sns.despine()
+fig.savefig("outputs/plots/active_learning.pdf", bbox_inches="tight")
